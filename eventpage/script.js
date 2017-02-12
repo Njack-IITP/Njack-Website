d5734c8cb74109b43db0ced04d6db424795aae32
rjs211 <srivatsa211@gmail.com>
2017-02-12 18:19:46 +0530

    some more addons

--- eventpage/script.js
+++ eventpage/script.js
@@ -61,3 +61,21 @@ rb4.click(function(){
     m4.removeClass('pactive').removeClass('ractive');m3.removeClass('pactive').removeClass('ractive');m2.removeClass('pactive').removeClass('ractive');m1.removeClass('pactive').removeClass('ractive');
   });
 });
+
+
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
